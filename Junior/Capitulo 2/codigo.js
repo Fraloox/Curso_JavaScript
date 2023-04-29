0537292d1ed6f737720dcf0e72966acaac2c9868
Fraloox <lautarofranco75@gmail.com>
2023-04-29 13:43:57 -0300

    guardado

---
+++ Junior/Capitulo 2/codigo.js
@@ -0,0 +1,36 @@
+// *** ARRAYS ASOSIATIVOS ***  
+
+// let pc = {
+//     nombre: "LautaroPC",
+//     procesador: "Intel Core I7",
+//     ram: "16GB",
+//     espacio: "1TB"
+// }
+
+// let nombre = pc["nombre"];
+// let procesador = pc["procesador"];
+// let ram = pc["ram"];
+// let espacio = pc["espacio"];
+
+// let frase = `el nombrede mi pc es: <b>${nombre}</b> </br>
+//              el procesador es: <b>${procesador}</b> </br>
+//              la memoria ram es: <b>${ram}</b> </br>
+//              el espacio en disco es de: <b>${espacio}</b> </br>`;
+
+
+// document.write(frase);
+
+
+// *** BUCLES ***  
+
+// let numero = 0;
+
+// do {  -> *** EJECUTA Y LUEGO PREGUNTA ***
+//     numero++;
+//     document.write(numero);
+//     if (numero == 1000) {
+//         break;  -> *** SALE DEL BUCLE ***F
+//     }
+
+// }
+// while (numero < 6)
